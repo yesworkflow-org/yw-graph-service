4f56004f7a8f59bdeecadee40cdbde9918fa6bdc
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-02 14:44:14 -0700

    Added Script model class and simple POST method handler.

--- src/main/java/org/yesworkflow/service/graph/controller/GraphServiceController.java
+++ src/main/java/org/yesworkflow/service/graph/controller/GraphServiceController.java
@@ -10,15 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.yesworkflow.service.graph.model.Script;
+
 @RestController
 @RequestMapping("/api/v1/")
 @EnableAutoConfiguration
 @CrossOrigin
 public class GraphServiceController {
 
+	@RequestMapping(value="graph/", method=RequestMethod.POST)
+    @ResponseBody
+	public String getCachedGraph(@RequestBody Script script) {
+		return "{ \"language\": \"" + script.getLanguage() + "\"}";
+	}
+
 	@RequestMapping(value="graph/cache/{id}", method=RequestMethod.GET)
     // @ResponseBody
-	public String getCachedGraph(@PathVariable Long id) { //@RequestBody String script) {
+	public String getCachedGraph(@PathVariable Long id) {
 		return "{ \"cached_graphviz_file\": " + id + "}";
 	}
 }
---
+++ src/main/java/org/yesworkflow/service/graph/model/Script.java
@@ -0,0 +1,30 @@
+package org.yesworkflow.service.graph.model;
+
+public class Script {
+    
+    private String language;
+    private String code;
+
+    public Script() {}
+
+    public Script(String language, String code) {
+        this.language = language;
+        this.code = code;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
