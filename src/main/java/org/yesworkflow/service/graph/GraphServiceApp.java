eff8c9e27b5db3e60ec52729ff728d60aa0320f8
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-01 17:43:16 -0700

    New spring boot application for serving YW graphs.

---
+++ src/main/java/org/yesworkflow/service/graph/GraphServiceApp.java
@@ -0,0 +1,13 @@
+package org.yesworkflow.service.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphServiceApp.class, args);
+    }
+
+}
---
+++ src/main/java/org/yesworkflow/service/graph/controller/GraphServiceController.java
@@ -0,0 +1,22 @@
+package org.yesworkflow.service.graph.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+@EnableAutoConfiguration
+public class GraphServiceController {
+
+	@RequestMapping(value="graph/cache/{id}", method=RequestMethod.GET)
+    // @ResponseBody
+	public String getCachedGraph(@PathVariable Long id) { //@RequestBody String script) {
+		return "cached graphviz file " + id;
+	}
+}
